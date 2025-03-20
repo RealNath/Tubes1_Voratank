f8c796f26031120459cbef2e4a691d2fbf820410
Nathan <therealnathanmc7383@gmail.com>
2025-03-20 15:19:58 +0700

    refactor: refreshed build

--- src/alternative-bots/VoraBot/obj/Debug/net9.0/VoraBot.AssemblyInfo.cs
+++ src/alternative-bots/VoraBot/obj/Debug/net9.0/VoraBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoraBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+706e106059be00c86fd8f7d037bb7a2d11e0397f")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoraBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoraBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
