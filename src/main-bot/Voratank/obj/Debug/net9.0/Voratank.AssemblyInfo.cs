0d543c818be52541a2d58fbec38be90072660cff
Nathan <therealnathanmc7383@gmail.com>
2025-03-24 10:23:38 +0700

    chore: refreshed build

--- src/main-bot/Voratank/obj/Debug/net9.0/Voratank.AssemblyInfo.cs
+++ src/main-bot/Voratank/obj/Debug/net9.0/Voratank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Voratank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c39c460f2fc77de85ce305fb539c71cdd3a7dfac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2de633b72e6c9001ddd7bedaaae0f60e81ce718")]
 [assembly: System.Reflection.AssemblyProductAttribute("Voratank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Voratank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
